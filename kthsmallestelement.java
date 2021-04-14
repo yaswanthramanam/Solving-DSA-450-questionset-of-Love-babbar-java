45717afedaf88e44db9436697c06cb462aeb36df
yaswanthramanam <63838667+yaswanthramanam@users.noreply.github.com>
2021-04-14 08:20:35 +0530

    Create kthsmallestelement.java

---
+++ kthsmallestelement.java
@@ -0,0 +1,11 @@
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        
+        Arrays.sort(arr);
+        return arr[k-1];
+        }
+        
+        
+     
+}
