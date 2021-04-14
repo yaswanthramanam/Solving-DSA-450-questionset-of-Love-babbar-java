601941bd8440ff338096400a4d04f4b2b7dd71dd
yaswanthramanam <63838667+yaswanthramanam@users.noreply.github.com>
2021-04-14 09:22:57 +0530

    Create sort012.java

---
+++ sort012.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        Arrays.sort(a);
+        //for(int i=0;i<n;i++){
+         //   System.out.print(a[i]+" ");
+        //}
+        
+
+        
+    }
+}
